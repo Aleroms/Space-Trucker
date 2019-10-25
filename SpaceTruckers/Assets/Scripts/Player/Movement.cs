a20ad8f4e197d01c0e6bb5af3ae8941817cd774d
Omer Celik <mail@ocelik.com>
2019-10-25 16:38:52 -0700

    Added camera script & changed movement scipt to keep player in bounds

--- SpaceTruckers/Assets/Scripts/Player/Movement.cs
+++ SpaceTruckers/Assets/Scripts/Player/Movement.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-	public float speed;
+    public float speed;
+
+    //Needed to keep the player within camera boundaries.
+    Vector2 playerSize;
+    Vector2 camExtents;
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camExtents = new Vector2(Camera.main.orthographicSize * Screen.width / Screen.height, Camera.main.orthographicSize);
+        playerSize = GetComponent<SpriteRenderer>().size * transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+        UpdateCameraBoundaryValues();
 
-		gameObject.transform.position = new Vector2(transform.position.x + (h * speed), transform.position.y + (v * speed));
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed), transform.position.y + (v * speed));
+
+        transform.position += Vector3.right * Time.deltaTime * 3;
     }
+
+
+    private void LateUpdate()
+    {
+        Vector3 playerPos = transform.position;
+        playerPos.x = Mathf.Clamp(playerPos.x, minX, maxX);
+        playerPos.y = Mathf.Clamp(playerPos.y, minY, maxY);
+        transform.position = playerPos;
+    }
+
+    private void UpdateCameraBoundaryValues()
+    {
+        minX = Camera.main.transform.position.x - camExtents.x + playerSize.x / 2;
+        maxX = Camera.main.transform.position.x + camExtents.x - playerSize.x / 2;
+        minY = Camera.main.transform.position.y - camExtents.y + playerSize.x / 2;
+        maxY = Camera.main.transform.position.y + camExtents.y - playerSize.x / 2;
+    }
+
 }
