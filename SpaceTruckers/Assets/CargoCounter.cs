9021722eae736a88cced26b29c9a2629e3e01e06
Daniel Qi <drqi@gmail.com>
2019-10-27 01:19:58 -0700

    added cargo and cargo counter i think hopefully no git fire lmfao

---
+++ SpaceTruckers/Assets/CargoCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CargoCounter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI counter;
+
+    public void UpdateCounter(int newCargo)
+    {
+        counter.text = newCargo.ToString();
+    }
+}
---
+++ SpaceTruckers/Assets/PlayerCargo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCargo : MonoBehaviour
+{
+    [SerializeField] CargoCounter cargo;
+    [SerializeField] int startingCargo = 0;
+
+    int currentCargo;
+    public int CurrentCargo
+    {
+        get { return currentCargo; }
+        set
+        {
+            currentCargo = value;
+            UpdateCargoAmount(currentCargo);
+        }
+    }
+    public delegate void UpdateCargo(int newCargo);
+    public event UpdateCargo UpdateCargoAmount;
+
+    private void Start()
+    {
+        UpdateCargoAmount += cargo.UpdateCounter;
+        CurrentCargo = startingCargo;
+    }
+
+    public void PickedUpCargo()
+    {
+        CurrentCargo += 1;
+    }
+
+}
