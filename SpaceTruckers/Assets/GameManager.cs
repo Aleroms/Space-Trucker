fd70733c3a8143f1b6dce7e038c0952f6ef22630
Omer Celik <mail@ocelik.com>
2019-11-01 16:12:15 -0700

    Game manager added & Player currency added & End base prefab added

---
+++ SpaceTruckers/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    int currency;
+
+    public int rewardPerCollectedCargo = 5;
+
+    
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+
+    public void IncreaseCurrencyBy(int income)
+    {
+        currency += income;
+        Debug.Log("Total User Money is now: " + currency);
+    }
+
+
+
+}
---
+++ SpaceTruckers/Assets/Scripts/EndBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBase : MonoBehaviour
+{
+
+    [SerializeField] GameObject endMenu; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endMenu.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            int cargoCount = collision.gameObject.GetComponent<PlayerCargo>().CurrentCargo;
+            Debug.Log(cargoCount);
+
+            endMenu.SetActive(true);
+        }
+    }
+}
