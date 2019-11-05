b359dbe263b346e1e28b24dd0af0ed6bdae93771
Daniel Qi <drqi@gmail.com>
2019-11-05 00:25:04 -0800

    Added prototype gun

---
+++ SpaceTruckers/Assets/Scripts/Weapons/BulletMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        rb.velocity = Vector2.right * speed;
+        this.transform.position = transform.parent.position;
+        this.transform.parent = null;
+    }
+
+    private void OnBecameInvisible()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+}
